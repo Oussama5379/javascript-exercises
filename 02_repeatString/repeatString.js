cbad8842596af11a3e776897e9d703607f99e107
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-01-31 16:52:24 +0100

    Complete the repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, numberOfRepeat) {
+  if (numberOfRepeat < 0) {
+    return "ERROR";
+  }
+  let result = "";
+  for (let i = 0; i < numberOfRepeat; i++) {
+    result += string;
+  }
+  return result;
 };
 
 // Do not edit below this line
