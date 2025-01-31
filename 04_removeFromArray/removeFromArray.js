57b0db767547bc5fd220e7154ac56c1d6b373eab
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-01-31 18:22:57 +0100

    Write the removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...moreArgs) {
+	for (const arg of moreArgs) {
+		array = array.filter((item) => item !== arg);
+	}
+
+	return array;
 };
 
 // Do not edit below this line
