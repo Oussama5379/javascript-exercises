60b543bc7433ea1edba109babc191769a4ddb71c
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-01-31 17:19:11 +0100

    Write a function that reverses a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+  let myArray = [];
+  myArray = text.split("");
+  myArray.reverse();
+  text = myArray.join("");
+  return text;
 };
 
 // Do not edit below this line
