75b308bedde743a87d2eb6f0e891746da6f1b093
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-02-01 11:48:21 +0100

    Write the sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+	if (a < b) {
+		num1 = a;
+		num2 = b;
+	} else {
+		num1 = b;
+		num2 = a;
+	}
+	if (num1 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+		return "ERROR";
+	}
+	let sum = 0;
+	for (let i = num1; i <= num2; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
