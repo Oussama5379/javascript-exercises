939c5bb68d4a37bf96b8a78999ce27c761889b65
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-02-05 16:25:32 +0100

    Write the palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	let result = string.replaceAll(",", "");
+	result = result.replaceAll(" ", "");
+	result = result.replaceAll(".", "");
+	result = result.replaceAll("!", "");
+	result = result.toUpperCase();
+	return result === result.split("").reverse().join("");
 };
 
 // Do not edit below this line
