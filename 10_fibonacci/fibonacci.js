2754cc6028e5fb493e5a9777b9f0bf4ab6396be4
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-02-05 17:10:23 +0100

    Create a function that returns a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (memberPosition) {
+	let f2 = 1,
+		f3 = 2;
+	if (memberPosition == 1 || memberPosition == 2) return 1;
+	if (memberPosition == 0) return 0;
+	if (memberPosition < 0) return "OOPS";
+	let pos = 3;
+	let aux;
+	while (memberPosition != pos) {
+		aux = f3;
+		f3 = f3 + f2;
+		f2 = aux;
+		pos++;
+	}
+	return f3;
 };
 
 // Do not edit below this line
