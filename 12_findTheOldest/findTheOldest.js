af3449dd6670da005234116f34744c98cddfa206
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-02-07 18:30:15 +0100

    Write the function that finds the oldest man in the array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+	const ages = array.map((person) => calculateAge(person));
+	const max = Math.max(...ages);
+	return array.find((person) => {
+		return calculateAge(person) == max;
+	});
 };
+function calculateAge(person) {
+	if (person.yearOfDeath == undefined) {
+		return new Date().getFullYear() - person.yearOfBirth;
+	}
+	return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
